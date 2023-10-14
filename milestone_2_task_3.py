3dfeb848d155ff3ef7154093a566d84d522a8b22
LouBidz <LouisaEMB@outlook.com>
2023-10-14 15:51:35 +0100

    updates task 3

--- milestone_2_task_3.py
+++ milestone_2_task_3.py
@@ -1,10 +1,21 @@
 import random # import 
 
-fruits = ['Apple','Grape','Strawberry','Banana','Melon'] #Step 1:Create a list containing the names of your 5 favorite fruits.
-word_list = fruits #Step 2:Assign this list to a variable called word_list.
-print(word_list)#prints out the list of fruits
-word = random.choice(word_list)#variable to hold the object random.choice
-print(word)#Print out the newly created list to the standard output (screen).
+# Step 1:Create a list containing the names of your 5 favorite fruits
+fruits = ['Apple','Grape','Strawberry','Banana','Melon'] 
+# Step 2:Assign this list to a variable called word_list.
+word_list = fruits 
+# prints out the list of fruits
+print(word_list)
+# Step 3:Create the random.choice method and pass the word_list variable into the choice method
+# Step 4:Assign the randomly generated word to a variable called word.
+word = random.choice(word_list)
+# Step 5: Print out word to the standard output. Run the code several times and observe the words printed out after each run.
+print(word)
 
-letter = input("Please enter a single letter: ")#Step 1:Using the input function, ask the user to enter a single letter
-guess = "You entered: " + str(letter)#Step 2:Assign the input to a variable called guess.
+# Step 1:Using the input function, ask the user to enter a single letter
+letter = input("Please enter a single letter: ")
+# Step 2:Assign the input to a variable called guess.
+guess = "You entered: " + str(letter)
+
+# This creates a list of fruits,chooses a random fruit and prints it.
+# Then it asks the user to choose a single letter from the alphabet and assigns it to a variable called guess.
