ff1e0d78f8ec9af373c88d58cf9abdbe69a86ca0
LouBidz <LouisaEMB@outlook.com>
2023-10-14 15:47:17 +0100

    Adds task 3

---
+++ milestone_2_task_3.py
@@ -0,0 +1,10 @@
+import random # import 
+
+fruits = ['Apple','Grape','Strawberry','Banana','Melon'] #Step 1:Create a list containing the names of your 5 favorite fruits.
+word_list = fruits #Step 2:Assign this list to a variable called word_list.
+print(word_list)#prints out the list of fruits
+word = random.choice(word_list)#variable to hold the object random.choice
+print(word)#Print out the newly created list to the standard output (screen).
+
+letter = input("Please enter a single letter: ")#Step 1:Using the input function, ask the user to enter a single letter
+guess = "You entered: " + str(letter)#Step 2:Assign the input to a variable called guess.
