eebc917499ecf4cd09c3dd351d6597b37b61f3bc
LouBidz <LouisaEMB@outlook.com>
2023-10-14 14:50:06 +0100

    updates task 1

--- milestone_2_task_1.py
+++ milestone_2_task_1.py
@@ -1,6 +1,6 @@
-#Step 1:Create a list containing the names of your 5 favorite fruits.
+# Step 1:Create a list containing the names of your 5 favorite fruits.
 fruits = ['Apple','Grape','Strawberry','Banana','Melon'] 
-#Step 2:Assign this list to a variable called word_list.
+# Step 2:Assign this list to a variable called word_list.
 word_list = fruits 
-#Step 3:Prints out the list of fruits
+# Step 3:Prints out the list of fruits
 print(word_list)
