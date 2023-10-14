6b768221a2723f558684b1b504dd9f810109ea43
LouBidz <LouisaEMB@outlook.com>
2023-10-14 09:09:09 +0100

    Adds task 2

---
+++ milestone_2_task_2.py
@@ -0,0 +1,8 @@
+import random # imports random 
+
+fruits = ['Apple','Grape','Strawberry','Banana','Melon'] #Step 1:Create a list containing the names of your 5 favorite fruits.
+word_list = fruits #Step 2:Assign this list to a variable called word_list.
+print(word_list)#prints out the list of fruits
+word = random.choice(word_list)#variable to hold the object random.choice
+print(word)#Print out the newly created list to the standard output (screen).
+
