2fbb3a06ebae6e81444c04c3d00db6e6f562b04e
LouBidz <LouisaEMB@outlook.com>
2023-10-14 14:51:14 +0100

    updates task 2

--- milestone_2_task_2.py
+++ milestone_2_task_2.py
@@ -1,8 +1,16 @@
 import random # imports random 
 
-fruits = ['Apple','Grape','Strawberry','Banana','Melon'] #Step 1:Create a list containing the names of your 5 favorite fruits.
-word_list = fruits #Step 2:Assign this list to a variable called word_list.
-print(word_list)#prints out the list of fruits
-word = random.choice(word_list)#variable to hold the object random.choice
-print(word)#Print out the newly created list to the standard output (screen).
+# Step 1:Create a list containing the names of your 5 favorite fruits
+fruits = ['Apple','Grape','Strawberry','Banana','Melon'] 
+# Step 2:Assign this list to a variable called word_list.
+word_list = fruits 
+# prints out the list of fruits
+print(word_list)
+# Step 3:Create the random.choice method and pass the word_list variable into the choice method
+# Step 4:Assign the randomly generated word to a variable called word.
+word = random.choice(word_list)
+# Step 5: Print out word to the standard output. Run the code several times and observe the words printed out after each run.
+print(word)
+
+# This looks in the list of fruits(word_list) and choses a random fruit each time its run 
 
