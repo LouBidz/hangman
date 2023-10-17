e730b9a216875c1632a0455afce4c1e81f19a66f
LouBidz <LouisaEMB@outlook.com>
2023-10-17 17:06:04 +0100

    Update task 4

--- milestone_4_task_4.py
+++ milestone_4_task_4.py
@@ -13,12 +13,12 @@ class Hangman:
  def check_guess(self, guess):
     guess = guess.lower()
     if guess in self.word:
+      for index, letter in enumerate(self.word):
+        if guess == letter: 
+           self.word_guessed[index] = guess
+           self.num_letters -= 1
         print(f"Good guess! {guess} is in the word")
-        for index, letter in enumerate(self.word):
-            if letter == guess:
-                self.word_guessed[index] = guess
-                self.num_letters -= 1
-
+        print(f' '.join(self.word_guessed))
 # Step 1:In the check_guess method, Create an else statement.
 # Step 2:Within the else block:
     else:
@@ -32,15 +32,21 @@ class Hangman:
  def ask_for_input(self):
     while True:
         guess = input("Guess a letter: ").lower()
-        if not guess.isalpha() or len(guess) != 1:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+        if guess in self.word:
+            for inx, letter in enumerate(self.word):
+                if guess == letter:
+                    self.word_guessed[inx] = guess
+                    self.num_letters -=1
         elif guess in self.list_of_guesses:
             print("You already tried that letter!")
+        elif not guess.isalpha() or len(guess) != 1:
+            print("Invalid letter. Please, enter a single alphabetical character.")
         else:
             self.check_guess(guess)
-            self.list_of_guesses.append(guess)
-        break
- ask_for_input()
+            break
+    self.list_of_guesses.append(guess)
+
+ 
 
 word_list = ['Apple','Grape','Strawberry','Banana','Melon'] 
 
